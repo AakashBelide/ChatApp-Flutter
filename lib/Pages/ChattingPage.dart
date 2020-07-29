f967c8af253ad9cf8a4ff27a18e059c1c25981a4
AakashBelide <aakash.belide@gmail.com>
2020-07-29 21:45:26 +0530

    Fixed context error

--- lib/Pages/ChattingPage.dart
+++ lib/Pages/ChattingPage.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:js';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
